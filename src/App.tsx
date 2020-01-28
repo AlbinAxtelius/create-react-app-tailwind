7ade8bf63a47e65f3db57f3e7a9a43cc7230e71c
Albin Axtelius <albin@axtelius.se>
2020-01-28 15:56:56 +0100

    🚸 Added small example and instructions

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <div className="w-64 h-32 p-2 bg-white rounded shadow">
+        <h1 className="text-2xl text-gray-800">Hello world</h1>
+        <p>
+          Configure tailwind by editing the{" "}
+          <code className="p-1 bg-gray-300 rounded select-all">
+            tailwind.config.js
+          </code>{" "}
+          file.
+        </p>
+      </div>
     </div>
   );
 };
