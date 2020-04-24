7bf73971abe708c36f221115916a73486e153bb2
Albin Axtelius <albin@axtelius.se>
2020-04-24 11:00:13 +0200

    ➕Added RxJS

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,20 @@
 import { RouteComponentProps } from "@reach/router";
 import React from "react";
+import { zip, chain, reduce, map, flatten, concat } from "lodash";
 
 const Home: React.FC<RouteComponentProps> = () => {
-  return (
-    <div className="flex">
-      <h1>Home</h1>
-    </div>
-  );
+  
+
+	return (
+		<div className="flex">
+			{JSON.stringify(
+				chain(concat([1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12])).reduce(
+					(a, b) => a + b,
+					0
+				)
+			)}
+		</div>
+	);
 };
 
 export default Home;
