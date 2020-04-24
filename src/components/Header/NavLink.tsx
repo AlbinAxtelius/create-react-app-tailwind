cf719da610f629871f2bc31f8ba566c080a26a4f
Albin Axtelius <albin@axtelius.se>
2020-04-24 10:58:44 +0200

    🎨 Change to use new space class in tailwindcss

--- src/components/Header/NavLink.tsx
+++ src/components/Header/NavLink.tsx
@@ -2,18 +2,18 @@ import { Link } from "@reach/router";
 import React from "react";
 
 export interface Props {
-  to: string;
+	to: string;
 }
 
-const NavLink: React.FC<Props> = props => (
-  <Link
-    getProps={({ isCurrent }) => ({
-      className: isCurrent
-        ? "p-2 px-4 font-bold uppercase rounded text-teal-600"
-        : "p-2 px-4 font-bold text-gray-600 uppercase rounded hover:text-teal-600 active:text-teal-800"
-    })}
-    {...props}
-  />
+const NavLink: React.FC<Props> = (props) => (
+	<Link
+		getProps={({ isCurrent }) => ({
+			className: isCurrent
+				? "font-bold uppercase rounded text-teal-600"
+				: "font-bold text-gray-600 uppercase rounded hover:text-teal-600 active:text-teal-800",
+		})}
+		{...props}
+	/>
 );
 
 export default NavLink;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,18 +5,18 @@ import React from "react";
 import NavLink from "./NavLink";
 
 const Header: React.FC = () => (
-  <div className="w-full shadow">
+  <header className="w-full shadow">
     <div className="sticky top-0 flex items-center justify-between h-16 max-w-screen-xl px-4 mx-auto xl:px-0">
       <Link to="/" className="flex items-center text-teal-600 ">
         <Icon path={mdiDeathStarVariant} className="w-8 h-8 fill-current" />
         <span className="ml-2 font-mono text-2xl text-gray-900">Template</span>
       </Link>
-      <nav>
+      <nav className="space-x-6">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
       </nav>
     </div>
-  </div>
+  </header>
 );
 
 export default Header;
