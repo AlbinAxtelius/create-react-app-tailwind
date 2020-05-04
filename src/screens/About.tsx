15ef757f015f43b3cfc8fae22fcd5a06fac3c4c7
Albin Axtelius <albin@axtelius.se>
2020-05-04 09:45:21 +0200

    🚧 Migrating to react-router-dom

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { RouteProps } from "react-router-dom";
 
-const About: React.FC<RouteProps> = () => {
+const About: React.FC = () => {
   return (
     <div>
       <h1>About</h1>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,6 @@
-import { mdiCreditCardOutline } from "@mdi/js";
-import Icon from "@mdi/react";
-import React, { useState } from "react";
-import InputMask from "react-input-mask";
-import { RouteProps } from "react-router-dom";
+import React from "react";
 
-const Home: React.FC<RouteProps> = () => {
-	const [creditCardInfo, setCreditCardInfo] = useState<string>("");
-
+const Home: React.FC = () => {
 	return (
 		<div className="flex flex-col">
 			<h1>Home</h1>
